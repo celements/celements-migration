d3f143b01efbffc2bbfb89b545f5b15e663127a9
Marc Sladek <marc@sladek.dev>
2023-05-25 17:27:18 +0200

    Prepare 6.0

--- src/test/java/com/celements/migrations/SubSystemHibernateMigrationManagerTest.java
+++ src/test/java/com/celements/migrations/SubSystemHibernateMigrationManagerTest.java
@@ -1,5 +1,6 @@
 package com.celements.migrations;
 
+import static com.celements.common.test.CelementsTestUtils.*;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
@@ -8,10 +9,8 @@ import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import org.xwiki.component.annotation.Component;
-import org.xwiki.component.descriptor.ComponentDescriptor;
-import org.xwiki.component.manager.ComponentManager;
 
-import com.celements.common.test.AbstractBridgedComponentTestCase;
+import com.celements.common.test.AbstractComponentTest;
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiConfig;
 import com.xpn.xwiki.XWikiContext;
@@ -20,10 +19,9 @@ import com.xpn.xwiki.store.XWikiHibernateBaseStore.HibernateCallback;
 import com.xpn.xwiki.store.XWikiHibernateStore;
 import com.xpn.xwiki.store.migration.XWikiDBVersion;
 import com.xpn.xwiki.store.migration.XWikiMigrationManagerInterface;
-import com.xpn.xwiki.web.Utils;
 
 public class SubSystemHibernateMigrationManagerTest
-    extends AbstractBridgedComponentTestCase{
+    extends AbstractComponentTest {
 
   private XWikiContext context;
   private SubSystemHibernateMigrationManager subSysHibMigManager;
@@ -35,9 +33,9 @@ public class SubSystemHibernateMigrationManagerTest
   public void setUp_SubSystemHibernateMigrationManagerTest() throws Exception {
     context = getContext();
     xwiki = getWikiMock();
-    hbmStoreMock = createMockAndAddToDefault(XWikiHibernateStore.class);
+    hbmStoreMock = createDefaultMock(XWikiHibernateStore.class);
     expect(xwiki.getHibernateStore()).andReturn(hbmStoreMock).anyTimes();
-    xwikiCfg = createMockAndAddToDefault(XWikiConfig.class);
+    xwikiCfg = createDefaultMock(XWikiConfig.class);
     expect(xwiki.getConfig()).andReturn(xwikiCfg).anyTimes();
   }
 
@@ -47,22 +45,14 @@ public class SubSystemHibernateMigrationManagerTest
     expect(hbmStoreMock.executeRead(same(context), eq(true),
         isA(HibernateCallback.class))).andReturn(new XWikiDBVersion(100)).atLeastOnce();
     expect(xwikiCfg.getPropertyAsList(eq("xwiki.store.migration.force"))).andReturn(
-        new String[]{
-          "com.celements.migrations.SubSystemHibernateMigrationManagerTest$TestMigrator1",
-          "com.celements.migrations.SubSystemHibernateMigrationManagerTest$TestMigrator2"
-          }).atLeastOnce();
-    ComponentManager componentManager = Utils.getComponentManager();
-    ComponentDescriptor<ITestMigrator> test1DescMock = createMockAndAddToDefault(
-        ComponentDescriptor.class);
-    expect(test1DescMock.getRole()).andReturn(ITestMigrator.class);
-    expect(test1DescMock.getRoleHint()).andReturn("migrator-test1");
-    ComponentDescriptor<ITestMigrator> test2DescMock = createMockAndAddToDefault(
-        ComponentDescriptor.class);
-    expect(test2DescMock.getRole()).andReturn(ITestMigrator.class);
-    expect(test2DescMock.getRoleHint()).andReturn("migrator-test2");
+        new String[] {
+            "com.celements.migrations.SubSystemHibernateMigrationManagerTest$TestMigrator1",
+            "com.celements.migrations.SubSystemHibernateMigrationManagerTest$TestMigrator2"
+        }).atLeastOnce();
+    registerComponentMock(ITestMigrator.class, "migrator-test1", new TestMigrator1());
+    registerComponentMock(ITestMigrator.class, "migrator-test2", new TestMigrator2());
+
     replayDefault();
-    componentManager.registerComponent(test1DescMock, new TestMigrator1());
-    componentManager.registerComponent(test2DescMock, new TestMigrator2());
     subSysHibMigManager = new SubSystemHibernateMigrationManager(context, "SubSysTest",
         ITestMigrator.class);
     Map<XWikiDBVersion, ?> forcedMigrations = subSysHibMigManager.getForcedMigrations(
@@ -74,51 +64,59 @@ public class SubSystemHibernateMigrationManagerTest
   @Component("migrator-test1")
   public class TestMigrator1 implements ITestMigrator {
 
+    @Override
     public String getName() {
       return null;
     }
 
+    @Override
     public String getDescription() {
       return null;
     }
 
+    @Override
     public XWikiDBVersion getVersion() {
       return new XWikiDBVersion(5);
     }
 
+    @Override
     public void migrate(XWikiMigrationManagerInterface manager, XWikiContext context)
-        throws XWikiException {
-    }
+        throws XWikiException {}
 
+    @Override
     public boolean shouldExecute(XWikiDBVersion startupVersion) {
       return false;
     }
-    
+
   }
 
   @Component("migrator-test2")
   public class TestMigrator2 implements ITestMigrator {
 
+    @Override
     public String getName() {
       return null;
     }
 
+    @Override
     public String getDescription() {
       return null;
     }
 
+    @Override
     public XWikiDBVersion getVersion() {
       return new XWikiDBVersion(2);
     }
 
+    @Override
     public void migrate(XWikiMigrationManagerInterface manager, XWikiContext context)
-        throws XWikiException {
-    }
+        throws XWikiException {}
 
+    @Override
     public boolean shouldExecute(XWikiDBVersion startupVersion) {
       return false;
     }
-    
+
   }
 
 }
